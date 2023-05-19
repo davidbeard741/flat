f4ff88157bb12c5e89fdb9cc2a4b6466413614bf
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-05-19 19:19:09 -0400

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -1,7 +1,7 @@
 import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.14/mod.ts' 
 
 const filename = Deno.args[0];
-let json = await readJSON(filename);
+const json = await readJSON(filename)
 console.log(json);
 
 const spyRates = JSON.parse(await Deno.readTextFile("./spy-hist.json"));
