5ceaf00f347c0f315acedd1d291ec27c50e20d2f
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-05-19 22:14:55 -0400

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -3,28 +3,17 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
 const filename = Deno.args[0];
 const json = await readJSON(filename);
 
-let data = [];
-let timeSeries = Object.values(json["Time Series (Daily)"]);
-timeSeries = timeSeries[0]; // Access the first element of the array
-for (let date in timeSeries) {
-  let entry = timeSeries[date];
-  data.push({
-    time: date,
-    open: entry["1. open"],
-    high: entry["2. high"],
-    low: entry["3. low"],
-    close: entry["4. close"]
-  });
-}
-data.sort((a, b) => new Date(a.time) - new Date(b.time));
-data = data.map((item) => ({
-  time: item.time,
-  open: item.open,
-  high: item.high,
-  low: item.low,
-  close: item.close,
+const timeSeries = json["Time Series (Daily)"];
+const data = Object.entries(timeSeries).map(([date, entry]) => ({
+  time: date,
+  open: parseFloat(entry["1. open"]),
+  high: parseFloat(entry["2. high"]),
+  low: parseFloat(entry["3. low"]),
+  close: parseFloat(entry["4. close"])
 }));
 
-const newFile = `spy-postprocessed.json`;
+data.sort((a, b) => new Date(a.time) - new Date(b.time));
+
+const newFile = 'spy-postprocessed.json';
 await writeJSON(newFile, data);
-console.log("Wrote post-process files");
+console.log('Wrote post-processed file: ' + newFile);
