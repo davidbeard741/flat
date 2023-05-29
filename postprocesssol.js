8629dc249625830d7cda97bd4b0110c77f60fc57
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-05-29 18:23:10 -0400

    Update postprocesssol.js

--- postprocesssol.js
+++ postprocesssol.js
@@ -2,6 +2,7 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
 
 const filename = Deno.args[0];
 const data = await readJSON(filename);
+data.sort((a, b) => a.entry[0] - b.entry[0]);
 
 const reformattedData = data.map(entry => ({
   time: Math.floor(entry[0] / 1000),  // Convert milliseconds to seconds
@@ -10,7 +11,7 @@ const reformattedData = data.map(entry => ({
   low: entry[3],
   close: entry[4]
 }));
-reformattedData.sort((a, b) => a.time - b.time);
+// reformattedData.sort((a, b) => a.time - b.time);
 
 const newFile = 'sol-postprocessed.json';
 await writeJSON(newFile, reformattedData);
